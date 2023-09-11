51619b81a519b8b7ffa2d182e4670845cfacbfb0
beephole <118709832+beephole@users.noreply.github.com>
2023-09-11 16:41:34 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from rembg import remove
+from pathlib import Path
+import os
+
+
+def process_images(input_folder):
+    output_folder = input_folder / 'output'
+    output_folder.mkdir(exist_ok=True)
+
+    for file in input_folder.glob('*'):
+        if file.suffix in ['.jpg', '.jpeg','png']:
+            output_path = output_folder / (file.stem + ".out.png")
+
+            with open(file, 'rb') as i:
+                with open(output_path, 'wb') as o:
+                    img_data = i.read()
+                    output = remove(img_data)
+                    o.write(output)
+
+
+if __name__ == "__main__":
+    current_folder = Path(os.getcwd())
+    process_images(current_folder)
