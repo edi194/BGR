78ae8b7b200ff3cb73fcf40c987230aadcec7025
Adrian <misiakadrian@gmail.com>
2024-01-29 19:12:14 +0100

    main: add model selector

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from rembg import remove
+from rembg import new_session, remove
 from pathlib import Path
+from pick import pick
 import os
 
 
 def process_images(input_folder):
+    title = 'Choose model which will be used for background removal: '
+    options = ['u2net', 'u2netp', 'u2net_human_seg', 'u2net_cloth_seg', 'silueta', 'isnet-general-use', 'isnet-anime', 'sam']
+    option, index = pick(options, title, indicator='=>', default_index=0)
+    selected_model = option
+
     output_folder = input_folder / 'output'
     output_folder.mkdir(exist_ok=True)
+    model_name = new_session(selected_model)
+    print('Processing files using model: ' + selected_model)
 
     for file in input_folder.glob('*'):
         if file.suffix in ['.jpg', '.jpeg','.png', '.JPG', '.JPEG','.PNG']:
@@ -15,11 +23,10 @@ def process_images(input_folder):
             with open(file, 'rb') as i:
                 with open(output_path, 'wb') as o:
                     img_data = i.read()
-                    output = remove(img_data)
+                    output = remove(img_data, session=model_name)
                     o.write(output)
                     print('File ' + file.stem + ' done')
-
-
+                    
 if __name__ == "__main__":
     current_folder = Path(os.getcwd())
     process_images(current_folder)
